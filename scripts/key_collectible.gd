f55c7127722af63b3747ee78639cd589122aced9
Corentinrmt <corentin.remot@gmail.com>
2025-12-02 14:22:23 +0100

    [UPDATE] other collectibles add score + change character sprite (proto)

--- scripts/key_collectible.gd
+++ scripts/key_collectible.gd
@@ -1,9 +1,11 @@
 extends CollectibleBase
 
+@export var _scores_datas : Scores_Datas
 
 func on_collect() -> void:
 	super()
 	Player.Instance.key_count += 1
+	ScoreManager._add_score(_scores_datas._key_collect)
 
 
 func _on_body_entered(body:Node2D) -> void:
--- scripts/score/scores_data.gd
+++ scripts/score/scores_data.gd
@@ -4,3 +4,4 @@ class_name Scores_Datas extends Resource
 @export var _door_open : int
 @export var _heart_collect :int
 @export var _gold_collect : int
+@export var _key_collect : int
