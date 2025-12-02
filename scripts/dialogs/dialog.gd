b04cfbf5517ff95d8fb828ae07fd68b1a54c1df0
Manon Wimmer <manonwimmer30@gmail.com>
2025-12-02 18:19:22 +0100

    [ADD] Progressive display of dialog text + can skip on click

--- scripts/dialogs/dialog.gd
+++ scripts/dialogs/dialog.gd
@@ -2,18 +2,28 @@ extends Control
 
 const TraceryScript = preload("res://scripts/dialogs/tracery.gd")
 
+# UI
 @export var text_label: RichTextLabel
 @export var name_label: RichTextLabel
 @export var dialog_btn: Button
+@export var type_speed:float = 0.02
 
+# NPC
 var current_npc : Node
 
+# Cut des phrases
 var sentences_cut : PackedStringArray
 var current_sentence_id: int = 0
+
+# Affichage progressif
+var is_typing: bool = false
+var full_sentence: String = ""
+var revealed_characters: int = 0
 
 func _ready():
 	hide_dialog()
 	
+#region Get Sentences
 func _get_and_show_current_state_text():
 	_get_current_state()
 	
@@ -46,12 +56,32 @@ func _get_array_sentences(sentences : String):
 	
 	for i in range(sentences_cut.size()):
 		sentences_cut[i] = sentences_cut[i].strip_edges()
-	
+#endregion
+
+#region Show text
 func _show_current_sentence_text():
-	var current_sentence = sentences_cut[current_sentence_id]
-	text_label.text = current_sentence
-	pass
+	full_sentence = sentences_cut[current_sentence_id]
+	revealed_characters = 0
+	text_label.text = ""
 	
+	is_typing = true
+	_start_typing()
+	
+func _start_typing():
+	# End typing
+	if revealed_characters >= full_sentence.length() or not is_typing:
+		text_label.text = full_sentence
+		is_typing = false
+		return
+
+	# Add character
+	revealed_characters += 1
+	text_label.text = full_sentence.substr(0, revealed_characters)
+
+	# Wait & recursive
+	await get_tree().create_timer(type_speed).timeout
+	_start_typing()  
+
 func _next_sentence():
 	current_sentence_id += 1
 	if (current_sentence_id <= sentences_cut.size() - 1):
@@ -59,7 +89,9 @@ func _next_sentence():
 	else:
 		current_sentence_id = 0
 		hide_dialog()
-	
+#endregion
+
+#region Current State & Quest
 func _get_current_state():
 	match current_npc.current_dialog_state:
 		current_npc.DialogState.NONE:
@@ -77,10 +109,17 @@ func _get_current_state():
 		
 func _check_quest_progress():
 	pass
-	
+#endregion
+
+#region Show / Hide & Pressed
 func _on_dialog_pressed() -> void:
-	_next_sentence()
-	
+	if is_typing:
+		# Skip typing
+		is_typing = false
+	else:
+		# Next
+		_next_sentence()
+		
 func show_dialog(npc : Node) -> void:
 	current_npc = npc
 	_get_and_show_current_state_text()
@@ -88,3 +127,4 @@ func show_dialog(npc : Node) -> void:
 	
 func hide_dialog() -> void:
 	dialog_btn.hide()
+#endregion
