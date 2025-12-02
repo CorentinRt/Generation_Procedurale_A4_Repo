dd0108426356a426aa606a332d18bd431f123893
milunax <clemto95@live.fr>
2025-12-02 11:15:17 +0100

    [ADD] Random room generation with weight

---
+++ scripts/room_generation/random_room.gd
@@ -0,0 +1,43 @@
+class_name RoomGeneration extends Node2D
+
+@export var _roomsInteriorsDict:Dictionary[PackedScene, int] = {}
+@export var _roomsExteriorsDict:Dictionary[PackedScene, int] = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var selectedInterior:PackedScene = _pickRandomElementFromDict(_roomsInteriorsDict)
+	var selectedExterior:PackedScene = _pickRandomElementFromDict(_roomsExteriorsDict)
+	
+	var interiorInstance = selectedInterior.instantiate()
+	var exteriorInstance = selectedExterior.instantiate()
+	add_child(interiorInstance)
+	add_child(exteriorInstance)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _pickRandomElementFromDict(dictionary: Dictionary[PackedScene, int] = {}) -> PackedScene:
+	var returnedElement:PackedScene
+	var dictionaryKeys = dictionary.keys()
+	var dictionaryValues = dictionary.values()
+	var totalWeight:int = 0
+	
+	for i in dictionaryValues.size():
+		totalWeight += dictionaryValues.get(i)
+	
+	var randomDictionaryWeight:int = randi_range(0, totalWeight)
+	
+	var weight:int = 0
+	var count:int = 0
+	for i in dictionaryValues.size():
+		weight += dictionaryValues[i]
+		if(weight >= randomDictionaryWeight):
+			break
+		count += 1
+	
+	returnedElement = dictionaryKeys.get(count)
+	
+	return returnedElement
