358b0cd1c6f1f5f661a7c2f39911a48bca4dc7ab
Manon Wimmer <manonwimmer30@gmail.com>
2025-12-08 15:06:08 +0100

    [ADD] Random min & max enemy count for kill enemy quest (temp 1 & 2)

--- scripts/quests/quest_combat.gd
+++ scripts/quests/quest_combat.gd
@@ -1,8 +1,12 @@
 class_name QuestCombat extends Quest
 
+# A mettre à 5 - 10 apres tests
+var target_min_value: int = 1
+var target_max_value: int = 2
+
 func _ready() -> void:
 	# Setup data
 	quest_data = load("res://resources/quests/qd_combat.tres")
-	target_value = 2 # temp fixe, random à mettre
+	target_value = randi_range(target_min_value, target_max_value)
 	
 	GameManager.on_kill_enemy.connect(add_one_target_value)
