305f434fd63052d40c863c8e781c204ee193471a
Manon Wimmer <manonwimmer30@gmail.com>
2025-12-08 11:22:21 +0100

    [ADD] Add one target value in quest (need to connect in event)

--- scripts/quests/quest.gd
+++ scripts/quests/quest.gd
@@ -6,3 +6,10 @@ var is_quest_completed: bool = false
 
 var target_value: int = 0
 var current_value: int = 0
+
+# ennemy killed, item found...
+func add_one_target_value():
+	print("Add one target value to quest")
+	current_value += 1
+	if current_value >= target_value:
+		is_quest_completed = true
--- scripts/quests/quest_combat.gd
+++ scripts/quests/quest_combat.gd
@@ -3,4 +3,7 @@ class_name QuestCombat extends Quest
 func _ready() -> void:
 	# Setup data
 	quest_data = load("res://resources/quests/qd_combat.tres")
-	target_value = 5
+	target_value = 2 # temp fixe, random à mettre
+	
+	# todo : connect to event enemy killed -> add_one_target_value()
+	
